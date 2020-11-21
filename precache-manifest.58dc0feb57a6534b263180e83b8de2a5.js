837cc71a5b45f5362432f03138e9d25225551c42
unknown <gopigaurav9@gmail.com>
2020-11-21 22:04:37 +0530

    Updates

---
+++ precache-manifest.58dc0feb57a6534b263180e83b8de2a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a72e74098807a1ce6d93d1f29ffda2",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "510c911da670a78eec84",
+    "url": "/twitter-clone/static/css/main.c4fe22be.chunk.css"
+  },
+  {
+    "revision": "dac04fbc9bdf6d93f409",
+    "url": "/twitter-clone/static/js/2.235d0703.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.235d0703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "510c911da670a78eec84",
+    "url": "/twitter-clone/static/js/main.8838386a.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
